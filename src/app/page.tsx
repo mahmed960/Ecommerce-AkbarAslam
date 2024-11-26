caaf3a37c8760711005de99eac083d42ba3ba985
mahmed960 <ahmedmirzaa8@gmail.com>
2024-11-26 10:03:24 +0500

    commit test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
 import Sliderr from './components/slider';
 import Products from './components/products';
 
-export default async function Home() {
-  // Use environment variable for API URL
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api/posts'; 
-  const res = await fetch(apiUrl); 
+async function getPosts() {
+  const apiUrl =
+    process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api/posts';
+  const res = await fetch(apiUrl);
+
+  if (!res.ok) {
+    console.error("Failed to fetch data from API");
+    return []; // Return empty array if fetch fails
+  }
+
   const posts = await res.json();
+  return posts;
+}
+
+export default async function Home() {
+  const posts = await getPosts();
 
   return (
     <>
-      <Sliderr/>
+      <Sliderr />
       <hr />
       <Products posts={posts} />
     </>
