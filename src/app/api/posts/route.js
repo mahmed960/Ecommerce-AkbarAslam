4e6df5164f41e7fee532fa51f9209d1b467e33ff
mahmed960 <ahmedmirzaa8@gmail.com>
2024-11-26 11:34:27 +0500

    api

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -2,11 +2,11 @@
 
 export async function GET() {
     const posts = [
-      { id: 1, title: 'Post 1', mainCategory: 'electronics', subCategory: 'laptops', slug: 'acer-laptop' },
-      { id: 2, title: 'Post 2', mainCategory: 'electronics', subCategory: 'laptops', slug: 'dell-laptop' },
-      { id: 3, title: 'Post 3', mainCategory: 'fashion', subCategory: 'shoes', slug: 'nike-shoes' },
-      { id: 4, title: 'Post 4', mainCategory: 'electronics', subCategory: 'phones', slug: 'iphone-14' },
-      { id: 5, title: 'Post 5', mainCategory: 'fashion', subCategory: 'clothing', slug: 'leather-jacket' },
+      { id: 1, title: 'Product 1', mainCategory: '11.11 Sale', subCategory: '', slug: 'Solene' },
+      { id: 2, title: 'Product 2', mainCategory: 'Co Ords', subCategory: '', slug: 'calista' },
+      { id: 3, title: 'Product 3', mainCategory: 'Lawn', subCategory: '', slug: 'calista-new' },
+      { id: 4, title: 'Product 4', mainCategory: 'Formals', subCategory: '', slug: 'calista-old' },
+      { id: 5, title: 'Product 5', mainCategory: 'Pret', subCategory: '', slug: 'leather-jacket' },
     ];
   
     return new Response(JSON.stringify(posts), { status: 200 });
