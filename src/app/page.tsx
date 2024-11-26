d54cefd702ee9ad12a61470260102a4d4201ac0d
mahmed960 <ahmedmirzaa8@gmail.com>
2024-11-26 09:19:42 +0500

    env update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Sliderr from './components/slider';
 import Products from './components/products';
 
+
 export default async function Home() {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
   const res = await fetch(`${baseUrl}/api/posts`);
